02b59823bd1a6875baa420e2c1d0f6a572aa52a8
flm8620 <flm8620@gmail.com>
2015-04-08 03:49:54 +0200

    normalize the proportion of Destination

--- Model.cpp
+++ Model.cpp
@@ -6,23 +6,23 @@ using namespace Imagine;
 using namespace std;
 
 
-const int nbportes = 30;
-const int nbprevoyants = 1000;
-const int nbconfort = 500;
-const int tempsstation = 30;
-const double propstresse = 0.8;
-const int debitentree = 50;
-const double ecarttype = 1;
-const int nbdestinations = 3;
+const int nbPortes = 30;
+const int nbPrevoyants = 1000;
+const int nbConfort = 500;
+const int tempsStation = 30;
+const double propStresse = 0.8;
+const int debitEntree = 50;
+const double ecartType = 1;
+const int nbDestinations = 3;
 const double sigma = 5.0;
 //tableau contenant le nombre de passagers dans le train
 double s[30];
-int Destination[nbdestinations];
-double proportiondestination[nbdestinations];
+int Destination[nbDestinations];
+double proportionDestination[nbDestinations];
 const int nbsorties=2;
 int sorties[nbsorties];
 //proportion des passagers qui arrive par chaque sortie
-double proportionsorties[nbsorties];
+double proportionSorties[nbsorties];
 //paramètre de la loi exponentielle
 const int lambda = 1;
 
@@ -38,39 +38,44 @@ void init_sorties(){
         cout << "pour la sortie" << i+1 << " (entier entre 0 et 29)" << endl;
 		cin >> sorties[i];
 		cout << "un double quelconque" << endl;
-		cin >> proportionsorties[i];
-		S += proportionsorties[i];
+        cin >> proportionSorties[i];
+        S += proportionSorties[i];
 	}
 	for (int i = 0; i < nbsorties; i++){
-		proportionsorties[i] = proportionsorties[i] / S;
+        proportionSorties[i] /= S;
 	}
 }
 void init_Destination(){
 	cout << "Designez la porte la plus pres de chaque destination favorite" << endl;
 	cout << "Donnez la proportion de gens qui veulent y aller" << endl;
-	for (int i = 0; i < nbdestinations; i++){
+    double S=0;
+    for (int i = 0; i < nbDestinations; i++){
         cout << "pour la porte" << i + 1 << " (un entier entre 0 et 29)" << endl;
 		cin >> Destination[i];
 		cout << "un double quelconque" << endl;
-		cin >> proportiondestination[i];
+        cin >> proportionDestination[i];
+        S+=proportionDestination[i];
 	}
+    for (int i = 0; i < nbsorties; i++){
+        proportionDestination[i] /= S;
+    }
 }
 
 void gotominlocal(){
 	for (int i = 0; i < nbsorties; i++){
-		for (int j = 0; j < proportionsorties[i] * nbconfort; j++){
+        for (int j = 0; j < proportionSorties[i] * nbConfort; j++){
 			if (sorties[i] == 1){
-				for (int k = 1; k < nbportes; k++){
+                for (int k = 1; k < nbPortes; k++){
 					if (s[k + 1] >= s[k]){ s[k] += 1; }
 				}
 			}
-			if (sorties[i] == nbportes){
-				for (int k = nbportes-1; k > 0; k--){
+            if (sorties[i] == nbPortes){
+                for (int k = nbPortes-1; k > 0; k--){
 					if (s[k - 1] >= s[k]){ s[k] += 1; }
 				}
 			}
-			if (sorties[i] < nbportes && sorties[i]>0){
-                for (int k = sorties[i]; k < max( nbportes - sorties[i], sorties[i] ); k++){
+            if (sorties[i] < nbPortes && sorties[i]>0){
+                for (int k = sorties[i]; k < max( nbPortes - sorties[i], sorties[i] ); k++){
 					if (s[sorties[i] + k + 1] >= s[sorties[i] + k]){ s[sorties[i] + k] += 1; }
 					else if (s[sorties[i] - k - 1] >= s[sorties[i] - k]){ s[sorties[i] - k] += 1; }
 				}
@@ -84,29 +89,29 @@ void loiexponentiel(int sorties[nbsorties]){
 	for (int i = 0; i < nbsorties; i++){
 		//Cas sortie au début du quai
 		if (sorties[i] == 0){
-			for (int j = 0; j < nbportes; j++){
-                double A=(1 - exp(-lambda)) / (1 - exp(-lambda*nbportes));
+            for (int j = 0; j < nbPortes; j++){
+                double A=(1 - exp(-lambda)) / (1 - exp(-lambda*nbPortes));
                 //A est une constante pour normer la loi
-                s[j] += A*exp(-lambda*j)   *   tempsstation*debitentree*propstresse*proportionsorties[i];
+                s[j] += A*exp(-lambda*j)   *   tempsStation*debitEntree*propStresse*proportionSorties[i];
 				//la loi est normée puis elle est multpliée par le nombre de passager stressé et en retard entrant par la sortie i.
             }
 		}
 		//Cas sortie à la fin du quai
-		if (sorties[i] == nbportes - 1){
-			for (int j = 0; j < nbportes; j++){
-                double A=(1 - exp(-lambda)) / (1 - exp(-lambda*nbportes))*exp(-lambda*(nbportes - j-1));
-                s[j] += A*tempsstation*debitentree*propstresse*proportionsorties[i];
+        if (sorties[i] == nbPortes - 1){
+            for (int j = 0; j < nbPortes; j++){
+                double A=(1 - exp(-lambda)) / (1 - exp(-lambda*nbPortes))*exp(-lambda*(nbPortes - j-1));
+                s[j] += A*tempsStation*debitEntree*propStresse*proportionSorties[i];
 			}
 		}
 		//Cas sortie au milieu du quai
-        if (sorties[i]>0 && sorties[i] < nbportes-1){
+        if (sorties[i]>0 && sorties[i] < nbPortes-1){
 			for (int j = 0; j < sorties[i]; j++){
-                double A=(1 - exp(-lambda)) / (1 + exp(1) - exp(-lambda*sorties[i]) - exp(-lambda*(nbportes - sorties[i])));
-                s[j] += A*exp(-lambda*(sorties[i] - 1 - j))*tempsstation*debitentree*propstresse*proportionsorties[i];
+                double A=(1 - exp(-lambda)) / (1 + exp(1) - exp(-lambda*sorties[i]) - exp(-lambda*(nbPortes - sorties[i])));
+                s[j] += A*exp(-lambda*(sorties[i] - 1 - j))*tempsStation*debitEntree*propStresse*proportionSorties[i];
 			}
-			for (int j = sorties[i]; j < nbportes; j++){
-                double A=(1 - exp(-lambda)) / (2 - exp(-lambda*sorties[i]) - exp(-lambda*(nbportes - sorties[i])));
-                s[j] += A*exp(-lambda*(j + 1 - sorties[i]))*tempsstation*debitentree*propstresse*proportionsorties[i];
+            for (int j = sorties[i]; j < nbPortes; j++){
+                double A=(1 - exp(-lambda)) / (2 - exp(-lambda*sorties[i]) - exp(-lambda*(nbPortes - sorties[i])));
+                s[j] += A*exp(-lambda*(j + 1 - sorties[i]))*tempsStation*debitEntree*propStresse*proportionSorties[i];
 			}
 			//cette expression est la synthèse de trois étapes
             //1/ à la porte la plus proche de la sortie, la fonction a pour valeur 1 et elle décroit de chaque côté selon une même loi exponentielle de paramètre -lambda
@@ -120,10 +125,10 @@ void loiuniforme(int sorties[nbsorties]){
 	//boucle sur toutes les sorties
 	for (int i = 0; i < nbsorties; i++){
 		//boucle sur les destinations
-		for (int j = 0; j < nbdestinations; j++){
+        for (int j = 0; j < nbDestinations; j++){
 			//boucle sur les portes entre la sortie et les destinations
 			for (int k = min(i, j); k < max(i, j); k++){
-                s[k] += tempsstation*debitentree*(1 - propstresse)*proportionsorties[i] * proportiondestination[j]/(j-i);
+                s[k] += tempsStation*debitEntree*(1 - propStresse)*proportionSorties[i] * proportionDestination[j]/(j-i);
 			}
 		}
 	}
@@ -135,13 +140,13 @@ void loinormale(int i, int j){
     NormalDistribution normal;
     double xSup=(j+1-Destination[i])/sigma;
     double xInf=(j - Destination[i])/sigma;
-    double value=nbprevoyants*proportiondestination[i] * (normal.getPhi(xSup) - normal.getPhi(xInf));
+    double value=nbPrevoyants*proportionDestination[i] * (normal.getPhi(xSup) - normal.getPhi(xInf));
     s[j] += value;
 }
 
 
 void init_quai(){
-	for (int i = 0; i < nbportes; i++){
+    for (int i = 0; i < nbPortes; i++){
 		s[i] = 0;
 	}
 }
@@ -157,8 +162,8 @@ int main(){
 	init_Destination();
 	init_sorties();
 
-	for (int i = 0; i < nbdestinations; i++){
-		for (int j = 0; j < nbportes; j++){
+    for (int i = 0; i < nbDestinations; i++){
+        for (int j = 0; j < nbPortes; j++){
 			loinormale(i, j);
 		}
 	}
@@ -180,7 +185,7 @@ int main(){
 
 
     openWindow(620, 600);
-        for (int i = 0; i < nbportes; i++){
+        for (int i = 0; i < nbPortes; i++){
             drawLine(20 * i+10, 599, 20 * i+10, 599.-s[i], MAGENTA);
             cout << s[i] << endl;
         }
